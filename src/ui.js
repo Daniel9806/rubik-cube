8c31cafbcdef766ad43735acdfe9a00cd368b9e2
Danielo <danielm98060509@gmail.com>
2025-12-28 20:59:43 -0300

    Fix: Fixing rotate modes.

--- src/ui.js
+++ src/ui.js
@@ -1,7 +1,6 @@
 export function bindUi({ cube, input, cameraController }) {
   const resetBtn = document.querySelector('#resetBtn');
   const scrambleBtn = document.querySelector('#scrambleBtn');
-  const gestureMode = document.querySelector('#gestureMode');
   const cameraSensitivity = document.querySelector('#cameraSensitivity');
 
   if (resetBtn) {
@@ -15,13 +14,6 @@ export function bindUi({ cube, input, cameraController }) {
     scrambleBtn.addEventListener('click', () => {
       if (cube.activeTurn) return;
       cube.scramble(22);
-    });
-  }
-
-  if (gestureMode && input?.setDragMode) {
-    input.setDragMode(gestureMode.value);
-    gestureMode.addEventListener('change', () => {
-      input.setDragMode(gestureMode.value);
     });
   }
 
